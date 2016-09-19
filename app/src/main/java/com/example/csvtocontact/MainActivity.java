60c03a7f5c79367ff1db5008310af9bfcc8b0cce
Vishal Khakhkhar <vdkhakhkhar@gmail.com>
2016-09-19 18:44:35 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/csvtocontact/MainActivity.java
@@ -0,0 +1,221 @@
+package com.example.csvtocontact;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends ListActivity {
+	ArrayList<MyContact> cArr = new ArrayList<MyContact>();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		boolean flg = readCSV("");
+
+		((ListView) findViewById(android.R.id.list))
+				.setAdapter(new MySimpleArrayAdapter(this));
+
+	}
+
+	public class MySimpleArrayAdapter extends BaseAdapter {
+		private final Context context;
+		
+
+		public MySimpleArrayAdapter(Context context) {
+			this.context = context;
+		}
+
+		/********* Create a holder Class to contain inflated xml file elements *********/
+		public class ViewHolder {
+
+			public TextView name;
+			public TextView mobile;
+			public TextView cell;
+
+		}
+
+		ViewHolder holder;
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			View vi = convertView;
+
+			if (convertView == null) {
+				LayoutInflater inflater = (LayoutInflater) context
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				/****** Inflate tabitem.xml file for each row ( Defined below ) *******/
+				vi = inflater.inflate(R.layout.row_list, null);
+
+				/****** View Holder Object to contain tabitem.xml file elements ******/
+
+				holder = new ViewHolder();
+				holder.name = (TextView) vi.findViewById(R.id.txtName);
+				holder.mobile = (TextView) vi.findViewById(R.id.txtMobile);
+				holder.cell = (TextView) vi.findViewById(R.id.txtCell);
+
+				/************ Set holder with LayoutInflater ************/
+				vi.setTag(holder);
+			} else
+				holder = (ViewHolder) vi.getTag();
+
+			holder.name.setText(cArr.get(position).name + "\n"
+					+ cArr.get(position).block + " - "
+					+ cArr.get(position).blockno);
+			holder.mobile.setText(cArr.get(position).mobile);
+			if(cArr.get(position).cell!= null && cArr.get(position).cell.length()>0){
+				holder.cell.setText(cArr.get(position).cell);
+			}
+			
+			holder.mobile.setTag(cArr.get(position).mobile);
+			holder.mobile.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + (String)v.getTag()));
+					startActivity(intent);
+				}
+			});
+			
+			holder.cell.setTag(cArr.get(position).cell);
+			holder.cell.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + (String)v.getTag()));
+					startActivity(intent);
+				}
+			});
+			
+			return vi;
+		}
+
+		
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return cArr.size();
+		}
+	}
+
+	private boolean readCSV(String path) {
+		cArr.clear();
+		try {
+
+			// create BufferedReader to read CSV file
+			// BufferedInputStream bri = new
+			// BufferedInputStream(getAssets().open(getAssets().list("")[0]));
+			// AssetFileDescriptor descriptor =
+			// getAssets().openFd("MRContacts.csv");
+			// FileReader reader = new
+			// FileReader(descriptor.getFileDescriptor());
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					getAssets().open("MRContacts.csv"), "UTF-8"));
+			// BufferedReader br = new BufferedReader( new
+			// FileReader(createFileFromInputStream(getAssets().open("MRContacts.csv"))));
+			String record = "";
+			StringTokenizer st = null;
+			int rowNumber = 0;
+			int cellIndex = 0;
+
+			// read comma separated file line by line
+			while ((record = br.readLine()) != null) {
+				rowNumber++;
+				MyContact c = new MyContact();
+				// break comma separated line using ","
+				st = new StringTokenizer(record, ",");
+				while (st.hasMoreTokens()) {
+					// display CSV values
+					cellIndex++;
+					String tmp = st.nextToken();
+					if (cellIndex == 2) {
+						c.name = tmp;
+					} else if (cellIndex == 3) {
+						c.block = tmp;
+					} else if (cellIndex == 4) {
+						c.blockno = tmp;
+					} else if (cellIndex == 5) {
+						c.mobile = tmp;
+					} else if (cellIndex == 6) {
+						c.cell = tmp;
+					}
+					System.out.println("Cell column index: " + cellIndex);
+					System.out.println("Cell Value: " + tmp);
+					
+					System.out.println("---");
+				}
+
+				cArr.add(c);
+
+				// reset cell Index number
+				cellIndex = 0;
+
+			}
+
+			System.out.println("rowNumber: " + rowNumber);
+
+		} catch (Exception e) {
+			System.out.println("Exception while reading csv file: " + e);
+		}
+		return true;
+	}
+
+	private File createFileFromInputStream(InputStream inputStream) {
+
+		try {
+			File f = new File("test.csv");
+			OutputStream outputStream = new FileOutputStream(f);
+			byte buffer[] = new byte[1024];
+			int length = 0;
+
+			while ((length = inputStream.read(buffer)) > 0) {
+				outputStream.write(buffer, 0, length);
+			}
+
+			outputStream.close();
+			inputStream.close();
+
+			return f;
+		} catch (IOException e) {
+			// Logging exception
+		}
+
+		return null;
+	}
+
+	public static class MyContact {
+		public String name, block, blockno, mobile, cell;
+	}
+}
